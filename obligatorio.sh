5094a9fc9c2ace842110160b3ee81c39f29b4e3b
demarcopy <96490772+demarcopy@users.noreply.github.com>
2025-05-10 15:56:34 -0300

    Add files via upload

---
+++ obligatorio.sh
@@ -0,0 +1,44 @@
+while true; do
+    echo "---------- Menu -----------"
+    echo "1) Propiedades de la carpeta"
+    echo "2) Renombrar archivos"
+    echo "3) Resumen del estado del disco duro"
+    echo "4) Buscar palabras en carpeta"
+    echo "5) Reporte del sistema"
+    echo "6) Guardar URL"
+    echo "7) Ingresar ruta"
+    echo "8) Salir del menu"
+    read opcion
+
+        case $opcion in
+        1)
+            echo "Opcion 1." #Test
+        ;;
+        2)
+            echo "Opcion 2." #Test
+        ;;
+        3)
+            echo "Opcion 3." #Test
+        ;;
+        4)
+            echo "Opcion 4." #Test
+        ;;
+        5)
+            echo "Opcion 5." #Test
+        ;;
+        6)
+            echo "Opcion 6." #Test
+        ;;
+        7)
+            echo "Opcion 7." #Test
+        ;;
+        8)
+            echo "Saliendo..."
+        ;;
+        *)
+            echo "Debe ingresar un codigo correcto"
+        ;;
+        esac
+
+    echo "   "
+done
